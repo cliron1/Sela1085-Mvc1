ab3f1236846617bc6761a6a57bc0e01b45419575
liron <liron@flame-ware.com>
2023-05-29 15:13:37 +0300

    UsersService

--- FirstMvc/Program.cs
+++ FirstMvc/Program.cs
@@ -1,27 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
+using FirstMvc.Services;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+WebApplicationBuilder builder;
 
-var app = builder.Build();
+{
+    builder = WebApplication.CreateBuilder(args);
 
-// Configure the HTTP request pipeline.
-if(!app.Environment.IsDevelopment()) {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    builder.Services.AddSingleton<IUsersService, UsersService>();
+
+} {
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if(!app.Environment.IsDevelopment()) {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
---
+++ FirstMvc/Services/UsersService.cs
@@ -0,0 +1,45 @@
+﻿using FirstMvc.Models;
+
+namespace FirstMvc.Services;
+
+public interface IUsersService {
+    void Add(User item);
+    IEnumerable<User> GetAll();
+    User GetById(int id);
+    void DeleteById(int id);
+}
+
+public class UsersService : IUsersService {
+    private readonly List<User> data;
+
+    public UsersService() {
+        data = new List<User> {
+            new User{ Id=1, Name = "Liron"},
+            new User{ Id=2, Name = "Mirit"},
+            new User{ Id=3, Name = "Gili"}
+        };
+    }
+
+    public IEnumerable<User> GetAll() {
+        return data;
+    }
+    public User GetById(int id) {
+        return findById(id);
+    }
+    public void Add(User item) {
+        item.Id = 1;
+        if(data.Any())
+            item.Id += data.Max(x => x.Id);
+        data.Add(item);
+    }
+
+    public void DeleteById(int id) {
+        var item = findById(id);
+        if(item != null)
+            data.Remove(item);
+    }
+
+    private User findById(int id) {
+        return data.FirstOrDefault(x => x.Id == id);
+    }
+}
