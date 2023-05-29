dcb3eb8698e2f9f84f16eb8b62e0ba6b67fca585
liron <liron@flame-ware.com>
2023-05-29 15:12:45 +0300

    Added Models

---
+++ FirstMvc/Models/Comment.cs
@@ -0,0 +1,7 @@
+﻿namespace FirstMvc.Models;
+
+public class Comment {
+    public int Id { get; set; }
+    public int PhotoId { get; set; }
+    public string Text { get; set; }
+}
---
+++ FirstMvc/Models/Photo.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstMvc.Models;
+
+public class Photo {
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Path { get; set; }
+
+    //public string Title { get; set; }
+    //public string Desc { get; set; }
+    //public string Tags { get; set; }
+
+    public List<Comment> Comments { get; set; }
+}
