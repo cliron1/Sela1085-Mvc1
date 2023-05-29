c54dc81bd14c7cf8c95c8d2cca836144278b11ff
liron <liron@flame-ware.com>
2023-05-29 15:14:09 +0300

    Users Controller + Views

--- FirstMvc/Controllers/HomeController.cs
+++ FirstMvc/Controllers/HomeController.cs
@@ -5,34 +5,10 @@ using System.Diagnostics;
 namespace FirstMvc.Controllers;
 
 public class HomeController : Controller {
-    // localhost:7777/
+
     public IActionResult Index() => View();
 
-    // localhost:7777/Home/Privacy
     public IActionResult Privacy() => View();
-
-    // localhost:7777/Home/Nir
-    public IActionResult Nir() {
-        var nir = new Person {
-            Id = 1,
-            Name = "Nir",
-            Age = 24,
-            Skills = new List<string> {
-                "Html", "Css", "js", "Asp.Net Core MVC"
-            }
-        };
-
-        ViewData["other"] = 77;
-        ViewBag.minAge = 18;
-
-        return View(nir);
-    }
-
-    // localhost:7777/Home/Oriel
-    public IActionResult Oriel() => View();
-
-
-
 
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ FirstMvc/Controllers/UsersController.cs
@@ -0,0 +1,80 @@
+﻿using FirstMvc.Models;
+using FirstMvc.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMvc.Controllers;
+
+public class UsersController : Controller {
+    private readonly IUsersService usersService;
+
+    public UsersController(IUsersService usersServiceFromDI) {
+        usersService = usersServiceFromDI;
+    }
+
+    // GET: Users
+    public IActionResult Index()
+        => View(usersService.GetAll());
+
+    // GET: Users/Details/5
+    public IActionResult Details(int id) {
+        var item = usersService.GetById(id);
+        if(item == null)
+            return NotFound();
+        return View(item);
+    }
+
+    // GET: Users/Create
+    public IActionResult Create() {
+        return View();
+    }
+
+    // POST: Users/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(User model) {
+        if(!ModelState.IsValid)
+            return View(model);
+
+        usersService.Add(model);
+        //return View(model);
+        return RedirectToAction(nameof(Index));
+    }
+
+    // GET: Users/Edit/5
+    public ActionResult Edit(int id) {
+        return View();
+    }
+
+    // POST: Users/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(int id, IFormCollection collection) {
+        try {
+            return RedirectToAction(nameof(Index));
+        } catch {
+            return View();
+        }
+    }
+
+    // GET: Users/Delete/5
+    [HttpGet]
+    public IActionResult Delete(int id)  {
+        var item = usersService.GetById(id);
+        if(item == null)
+            return RedirectToAction(nameof(Index));
+        return View(item);
+    }
+
+    // POST: Users/Delete/5
+    [HttpPost("[controller]/Delete/{id}")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteAction(int id) {
+        try {
+            usersService.DeleteById(id);
+            return RedirectToAction(nameof(Index));
+        } catch {
+            return View();
+        }
+    }
+}
