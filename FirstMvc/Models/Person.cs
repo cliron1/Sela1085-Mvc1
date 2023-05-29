5205bff4cbcad065f003f8fe12cd3ba5f48f6cc8
liron <liron@flame-ware.com>
2023-05-29 09:40:42 +0300

    restored Person.cs

---
+++ FirstMvc/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace FirstMvc.Models;
+
+public class Person {
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string Phone { get; set; }
+
+    public int Age { get; set; }
+
+    public List<string> Skills { get; set; }
+}
